bf5ca74bec8461f64434b79bfea215d350d947e8
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-09-02 16:22:18 +0200

    first commit

---
+++ HW_testing.py
@@ -0,0 +1,15 @@
+class BankNumber:
+    def __init__(self, real_part, float_part):
+        self.real_part = real_part
+        self.float_part = float_part
+
+    def __eq__(self, other):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    # a ostatne znamienka
+
+    def __str__(self):
+        pass
